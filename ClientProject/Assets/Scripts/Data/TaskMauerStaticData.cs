73b6443bca34770d0a00ddc7ac941a752bb75acc
ndark <ndarkteng@gmail.com>
2018-11-03 10:58:19 +0400

    add TaskMauerStaticData.cs

---
+++ ClientProject/Assets/Scripts/Data/TaskMauerStaticData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TaskMauerStaticData
+{
+	public static int s_UpdateSerial = 0 ;
+
+	public static int GetUpdateSerial()
+	{
+		return s_UpdateSerial;
+	}
+
+	public static void SetUpdateSerial( int set )
+	{
+		Debug.Log("SetUpdateSerial() set=" + set);
+		s_UpdateSerial = set ;
+	}
+}
